f51d77fc2c1a14a7755030b0333670ae606206ec
Crunch (Chaz9) <charliephilp@outlook.com>
2024-03-02 20:20:31 +0000

    3 air

--- sonic3air-main/Oxygen/sonic3air/build/_xcode/boost.framework/Versions/A/Headers/config.hpp
+++ sonic3air-main/Oxygen/sonic3air/build/_xcode/boost.framework/Versions/A/Headers/config.hpp
@@ -1,67 +1,21 @@
-//  Boost config.hpp configuration header file  ------------------------------//
 
-//  (C) Copyright John Maddock 2002.
-//  Use, modification and distribution are subject to the 
-//  Boost Software License, Version 1.0. (See accompanying file 
-//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-
-//  See http://www.boost.org/libs/config for most recent version.
-
-//  Boost config.hpp policy and rationale documentation has been moved to
-//  http://www.boost.org/libs/config
+//  (C) Copyright Steve Cleary, Beman Dawes, Howard Hinnant & John Maddock 2000.
+//  Use, modification and distribution are subject to the Boost Software License,
+//  Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt).
 //
-//  CAUTION: This file is intended to be completely stable -
-//           DO NOT MODIFY THIS FILE!
+//  See http://www.boost.org/libs/type_traits for most recent version including documentation.
+
+#ifndef BOOST_TT_OLD_CONFIG_HPP_INCLUDED
+#define BOOST_TT_OLD_CONFIG_HPP_INCLUDED
+
 //
-
-#ifndef BOOST_CONFIG_HPP
-#define BOOST_CONFIG_HPP
-
-// if we don't have a user config, then use the default location:
-#if !defined(BOOST_USER_CONFIG) && !defined(BOOST_NO_USER_CONFIG)
-#  define BOOST_USER_CONFIG <boost/config/user.hpp>
-#if 0
-// For dependency trackers:
-#  include <boost/config/user.hpp>
-#endif
-#endif
-// include it first:
-#ifdef BOOST_USER_CONFIG
-#  include BOOST_USER_CONFIG
+// This header is deprecated and no longer used by type_traits:
+//
+#if defined(__GNUC__) || defined(_MSC_VER)
+# pragma message("NOTE: Use of this header (boost/type_traits/config.hpp) is deprecated")
 #endif
 
-// if we don't have a compiler config set, try and find one:
-#if !defined(BOOST_COMPILER_CONFIG) && !defined(BOOST_NO_COMPILER_CONFIG) && !defined(BOOST_NO_CONFIG)
-#  include <boost/config/detail/select_compiler_config.hpp>
-#endif
-// if we have a compiler config, include it now:
-#ifdef BOOST_COMPILER_CONFIG
-#  include BOOST_COMPILER_CONFIG
-#endif
+#endif // BOOST_TT_CONFIG_HPP_INCLUDED
 
-// if we don't have a std library config set, try and find one:
-#if !defined(BOOST_STDLIB_CONFIG) && !defined(BOOST_NO_STDLIB_CONFIG) && !defined(BOOST_NO_CONFIG) && defined(__cplusplus)
-#  include <boost/config/detail/select_stdlib_config.hpp>
-#endif
-// if we have a std library config, include it now:
-#ifdef BOOST_STDLIB_CONFIG
-#  include BOOST_STDLIB_CONFIG
-#endif
 
-// if we don't have a platform config set, try and find one:
-#if !defined(BOOST_PLATFORM_CONFIG) && !defined(BOOST_NO_PLATFORM_CONFIG) && !defined(BOOST_NO_CONFIG)
-#  include <boost/config/detail/select_platform_config.hpp>
-#endif
-// if we have a platform config, include it now:
-#ifdef BOOST_PLATFORM_CONFIG
-#  include BOOST_PLATFORM_CONFIG
-#endif
-
-// get config suffix code:
-#include <boost/config/detail/suffix.hpp>
-
-#ifdef BOOST_HAS_PRAGMA_ONCE
-#pragma once
-#endif
-
-#endif  // BOOST_CONFIG_HPP
--- sonic3air-main/README.md
+++ sonic3air-main/README.md
@@ -1,13 +1,17 @@
-## Discord Game SDK
 
-> The SDK is currently under extensive development and is subject to change. Suggestions
-> about the current API are welcome.
+# Simple DirectMedia Layer (SDL) Version 2.0
 
-### Setup
+https://www.libsdl.org/
 
-- Create an application on the Discord [developer site](https://discordapp.com/developers/applications/me).
-- Set a redirect URL. If you don't have one right now, just use <http://127.0.0.1>.
-- Enable Rich Presence for the application. This enables whitelist access for the SDK.
-  - When you are ready to test with more people, add them to the whitelist.
-- Copy the **Client ID**.
-  - Use this `CLIENT_ID` when initializing the SDK.
+Simple DirectMedia Layer is a cross-platform development library designed
+to provide low level access to audio, keyboard, mouse, joystick, and graphics
+hardware via OpenGL and Direct3D. It is used by video playback software,
+emulators, and popular games including Valve's award winning catalog
+and many Humble Bundle games.
+
+More extensive documentation is available in the docs directory, starting
+with README.md
+
+Enjoy!
+
+Sam Lantinga (slouken@libsdl.org)
